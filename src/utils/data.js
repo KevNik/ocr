1336823ffbf750d9f7eb4d81bd26d308aa6e3f23
Keven Wendler <keven.nikolas123@gmail.com>
2022-10-07 16:02:50 -0400

    revert alteração em timezone

--- src/utils/data.js
+++ src/utils/data.js
@@ -4,6 +4,6 @@ import timezone from "dayjs/plugin/utc.js"
 dayjs.extend(utc)
 dayjs.extend(timezone)
 
-dayjs.tz('America/Campo_Grande')
+dayjs.setDefault('America/Campo_Grande')
 
 export default dayjs
